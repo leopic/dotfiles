1a8db2437282d56d21a6574bc25b4dfd6a750a16
LEOPiC <leopic@gmail.com>
2012-09-12 20:51:31 -0600

    Cleanup.

--- compile.sh
+++ compile.sh
@@ -3,21 +3,16 @@
 # recibiendo la variable
 POSPAR1="$1"
 
+# parseando la extension
 ext=$(echo ${1}|awk -F\. '{print $2}')
 
+# si la extension es cpp
 if [ ${ext} == "cpp" ]; then
-  clear 
+  clear
   # compile
   g++ $1 -o app
   # execute
   ./app
 fi
 
-# clear 
-# compile
-# g++ $1 -o app
-# execute
-# ./app
-
-# http://stackoverflow.com/questions/4627701/vim-how-to-execute-automatically-execute-a-shell-command-after-saving-a-file
-# :autocmd BufWritePost * !run_tests.sh <afile>
+# enjoy
